3703126e7c46267e84a422087e977e57f59fa215
Ikhwan22 <ikhwannabila47@gmail.com>
2020-05-28 02:35:23 +0700

    revisi file sql

--- hafizclass.sql
+++ hafizclass.sql
@@ -1,134 +1,257 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     27/05/20 23:43:16                            */
-/*==============================================================*/
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2020 at 09:34 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-drop table if exists HARIAN;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-drop table if exists KOMENTAR;
+--
+-- Database: `hafizclass`
+--
 
-drop table if exists PENGUJI;
+-- --------------------------------------------------------
 
-drop table if exists PROGRESS;
+--
+-- Table structure for table `harian`
+--
 
-drop table if exists SANTRI;
+CREATE TABLE `harian` (
+  `ID_HARIAN` int(11) NOT NULL,
+  `ID_PROGRESS` int(11) NOT NULL,
+  `STATUS_HARIAN` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-drop table if exists TARGET;
+-- --------------------------------------------------------
 
-drop table if exists USTADZ;
+--
+-- Table structure for table `komentar`
+--
 
-/*==============================================================*/
-/* Table: HARIAN                                                */
-/*==============================================================*/
-create table HARIAN
-(
-   ID_HARIAN            int not null,
-   ID_PROGRESS          int not null,
-   STATUS_HARIAN        varchar(25),
-   primary key (ID_HARIAN)
-);
+CREATE TABLE `komentar` (
+  `ID_KOMEN` int(11) NOT NULL,
+  `ID_PROGRESS` int(11) NOT NULL,
+  `STATUS_KOMEN` varchar(20) DEFAULT NULL,
+  `NAMA_KOMEN` varchar(35) DEFAULT NULL,
+  `ISI_KOMEN` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*==============================================================*/
-/* Table: KOMENTAR                                              */
-/*==============================================================*/
-create table KOMENTAR
-(
-   ID_KOMEN             int not null,
-   ID_PROGRESS          int not null,
-   STATUS_KOMEN         varchar(20),
-   NAMA_KOMEN           varchar(35),
-   ISI_KOMEN            text,
-   primary key (ID_KOMEN)
-);
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: PENGUJI                                               */
-/*==============================================================*/
-create table PENGUJI
-(
-   ID_PENGUJI           int not null,
-   ID_USTADZ            int not null,
-   ID_SANTRI            int not null,
-   KETERANGAN_MENGUJI   varchar(25),
-   primary key (ID_PENGUJI)
-);
+--
+-- Table structure for table `penguji`
+--
 
-/*==============================================================*/
-/* Table: PROGRESS                                              */
-/*==============================================================*/
-create table PROGRESS
-(
-   ID_PROGRESS          int not null,
-   ID_SANTRI            int not null,
-   ID_USTADZ            int not null,
-   JUDUL_PROGRESS       varchar(50),
-   AUDIO                varchar(50),
-   TARGET               varchar(20),
-   primary key (ID_PROGRESS)
-);
+CREATE TABLE `penguji` (
+  `ID_PENGUJI` int(11) NOT NULL,
+  `EMAIL_PENGUJI` varchar(50) DEFAULT NULL,
+  `PASSWORD_PENGUJI` varchar(25) DEFAULT NULL,
+  `NAMA_PENGUJI` varchar(50) DEFAULT NULL,
+  `JK_PENGUJI` varchar(20) DEFAULT NULL,
+  `TELEPON_PENGUJI` int(11) DEFAULT NULL,
+  `FOTO_PENGUJI` varchar(50) DEFAULT NULL,
+  `TINGKAT_MENGUJI` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*==============================================================*/
-/* Table: SANTRI                                                */
-/*==============================================================*/
-create table SANTRI
-(
-   ID_SANTRI            int not null,
-   EMAIL_SANTRI         varchar(50),
-   PASSWORD_SANTRI      varchar(25),
-   NAMA_SANTRI          varchar(50),
-   JK_SANTRI            varchar(20),
-   TELEPON_SANTRI       int,
-   FOTO_SANTRI          varchar(50),
-   primary key (ID_SANTRI)
-);
+--
+-- Dumping data for table `penguji`
+--
 
-/*==============================================================*/
-/* Table: TARGET                                                */
-/*==============================================================*/
-create table TARGET
-(
-   ID_TARGET            int not null,
-   ID_PROGRESS          int not null,
-   STATUS_TARGET        varchar(25),
-   TANGGAL_UPLOAD       date,
-   BATAS_UPLOAD         date,
-   primary key (ID_TARGET)
-);
+INSERT INTO `penguji` (`ID_PENGUJI`, `EMAIL_PENGUJI`, `PASSWORD_PENGUJI`, `NAMA_PENGUJI`, `JK_PENGUJI`, `TELEPON_PENGUJI`, `FOTO_PENGUJI`, `TINGKAT_MENGUJI`) VALUES
+(1, 'penguji@gmail.com', '54321', 'akbar', 'lak laki', 23123123, 'bbb', NULL),
+(2, 'aaa@gmail.com', 'aaa', 'aaa', NULL, NULL, NULL, NULL);
 
-/*==============================================================*/
-/* Table: USTADZ                                                */
-/*==============================================================*/
-create table USTADZ
-(
-   ID_USTADZ            int not null,
-   EMAIL_USTADZ         varchar(50),
-   PASSWORD_USTADZ      varchar(25),
-   NAMA_USTADZ          varchar(50),
-   JK_USTADZ            varchar(20),
-   TELEPON_USTADZ       int,
-   FOTO_USTADZ          varchar(50),
-   primary key (ID_USTADZ)
-);
+-- --------------------------------------------------------
 
-alter table HARIAN add constraint FK_MEMILIKI_HARIAN foreign key (ID_PROGRESS)
-      references PROGRESS (ID_PROGRESS) on delete restrict on update restrict;
+--
+-- Table structure for table `progress`
+--
 
-alter table KOMENTAR add constraint FK_MEMBERIKAN foreign key (ID_PROGRESS)
-      references PROGRESS (ID_PROGRESS) on delete restrict on update restrict;
+CREATE TABLE `progress` (
+  `ID_PROGRESS` int(11) NOT NULL,
+  `ID_SANTRI` int(11) NOT NULL,
+  `ID_USTADZ` int(11) NOT NULL,
+  `JUDUL_PROGRESS` varchar(50) DEFAULT NULL,
+  `AUDIO` varchar(50) DEFAULT NULL,
+  `TARGET` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-alter table PENGUJI add constraint FK_DIUJI foreign key (ID_SANTRI)
-      references SANTRI (ID_SANTRI) on delete restrict on update restrict;
+-- --------------------------------------------------------
 
-alter table PENGUJI add constraint FK_MENGUJI foreign key (ID_USTADZ)
-      references USTADZ (ID_USTADZ) on delete restrict on update restrict;
+--
+-- Table structure for table `santri`
+--
 
-alter table PROGRESS add constraint FK_MEMBERI foreign key (ID_USTADZ)
-      references USTADZ (ID_USTADZ) on delete restrict on update restrict;
+CREATE TABLE `santri` (
+  `ID_SANTRI` int(11) NOT NULL,
+  `ID_PENGUJI` int(11) NOT NULL,
+  `EMAIL_SANTRI` varchar(50) DEFAULT NULL,
+  `PASSWORD_SANTRI` varchar(25) DEFAULT NULL,
+  `NAMA_SANTRI` varchar(50) DEFAULT NULL,
+  `JK_SANTRI` varchar(20) DEFAULT NULL,
+  `TINGKAT_PENDIDIKAN` varchar(10) NOT NULL,
+  `TELEPON_SANTRI` int(11) DEFAULT NULL,
+  `FOTO_SANTRI` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-alter table PROGRESS add constraint FK_TUGAS_SANTRI foreign key (ID_SANTRI)
-      references SANTRI (ID_SANTRI) on delete restrict on update restrict;
+--
+-- Dumping data for table `santri`
+--
 
-alter table TARGET add constraint FK_MEMILIKI_TARGET foreign key (ID_PROGRESS)
-      references PROGRESS (ID_PROGRESS) on delete restrict on update restrict;
+INSERT INTO `santri` (`ID_SANTRI`, `ID_PENGUJI`, `EMAIL_SANTRI`, `PASSWORD_SANTRI`, `NAMA_SANTRI`, `JK_SANTRI`, `TINGKAT_PENDIDIKAN`, `TELEPON_SANTRI`, `FOTO_SANTRI`) VALUES
+(1, 1, 'santri@gmail.com', '12345', 'ikhwan', 'laki-laki', 'SMA', 12321321, 'aaa'),
+(2, 1, 'asha@gmail.com', 'asha', 'asha', 'Perempuan', 'SD', NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `target`
+--
+
+CREATE TABLE `target` (
+  `ID_TARGET` int(11) NOT NULL,
+  `ID_PROGRESS` int(11) NOT NULL,
+  `STATUS_TARGET` varchar(25) DEFAULT NULL,
+  `TANGGAL_UPLOAD` date DEFAULT NULL,
+  `BATAS_UPLOAD` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `harian`
+--
+ALTER TABLE `harian`
+  ADD PRIMARY KEY (`ID_HARIAN`),
+  ADD KEY `ID_PROGRESS` (`ID_PROGRESS`);
+
+--
+-- Indexes for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD PRIMARY KEY (`ID_KOMEN`),
+  ADD KEY `ID_PROGRESS` (`ID_PROGRESS`);
+
+--
+-- Indexes for table `penguji`
+--
+ALTER TABLE `penguji`
+  ADD PRIMARY KEY (`ID_PENGUJI`),
+  ADD UNIQUE KEY `EMAIL_PENGUJI` (`EMAIL_PENGUJI`);
+
+--
+-- Indexes for table `progress`
+--
+ALTER TABLE `progress`
+  ADD PRIMARY KEY (`ID_PROGRESS`),
+  ADD KEY `ID_SANTRI` (`ID_SANTRI`),
+  ADD KEY `ID_USTADZ` (`ID_USTADZ`);
+
+--
+-- Indexes for table `santri`
+--
+ALTER TABLE `santri`
+  ADD PRIMARY KEY (`ID_SANTRI`),
+  ADD UNIQUE KEY `EMAIL_SANTRI` (`EMAIL_SANTRI`),
+  ADD KEY `santri_ibfk_1` (`ID_PENGUJI`);
+
+--
+-- Indexes for table `target`
+--
+ALTER TABLE `target`
+  ADD PRIMARY KEY (`ID_TARGET`),
+  ADD KEY `ID_PROGRESS` (`ID_PROGRESS`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `harian`
+--
+ALTER TABLE `harian`
+  MODIFY `ID_HARIAN` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `komentar`
+--
+ALTER TABLE `komentar`
+  MODIFY `ID_KOMEN` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penguji`
+--
+ALTER TABLE `penguji`
+  MODIFY `ID_PENGUJI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `progress`
+--
+ALTER TABLE `progress`
+  MODIFY `ID_PROGRESS` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `santri`
+--
+ALTER TABLE `santri`
+  MODIFY `ID_SANTRI` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `target`
+--
+ALTER TABLE `target`
+  MODIFY `ID_TARGET` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `harian`
+--
+ALTER TABLE `harian`
+  ADD CONSTRAINT `harian_ibfk_1` FOREIGN KEY (`ID_PROGRESS`) REFERENCES `progress` (`ID_PROGRESS`);
+
+--
+-- Constraints for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`ID_PROGRESS`) REFERENCES `progress` (`ID_PROGRESS`);
+
+--
+-- Constraints for table `progress`
+--
+ALTER TABLE `progress`
+  ADD CONSTRAINT `progress_ibfk_1` FOREIGN KEY (`ID_SANTRI`) REFERENCES `santri` (`ID_SANTRI`),
+  ADD CONSTRAINT `progress_ibfk_2` FOREIGN KEY (`ID_USTADZ`) REFERENCES `penguji` (`ID_PENGUJI`);
+
+--
+-- Constraints for table `santri`
+--
+ALTER TABLE `santri`
+  ADD CONSTRAINT `santri_ibfk_1` FOREIGN KEY (`ID_PENGUJI`) REFERENCES `penguji` (`ID_PENGUJI`);
+
+--
+-- Constraints for table `target`
+--
+ALTER TABLE `target`
+  ADD CONSTRAINT `target_ibfk_1` FOREIGN KEY (`ID_PROGRESS`) REFERENCES `progress` (`ID_PROGRESS`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
